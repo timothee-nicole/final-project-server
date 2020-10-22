a2b71b757d749de2c569ff20aa07561690a5f00c
Timothée Nicole <timothee.nicole@gmail.com>
2020-10-22 10:57:48 +0200

    first deploy

---
+++ public/precache-manifest.d28d960a63b9a3ca0b1ea62048559cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad69c5d02202ec2398459a3ecc3a88b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "795dd2a1f2840af52e1b",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "af55f47431596dd60852",
+    "url": "/static/css/main.97f5e7a4.chunk.css"
+  },
+  {
+    "revision": "795dd2a1f2840af52e1b",
+    "url": "/static/js/2.c58ebfef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c58ebfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af55f47431596dd60852",
+    "url": "/static/js/main.97bb62f0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
